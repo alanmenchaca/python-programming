0e721e44b453b8319693aa8ee002473e22027ab4
Fco. Alan Menchaca M <alanmenchaca22@outlook.com>
2020-11-08 04:00:12 -0600

    a simple attempt to model a dog, classes (Object-oriented programming)

--- classes/dog.py
+++ classes/dog.py
@@ -1,4 +1,4 @@
-"""Classes (Object-oriented programming) 
+"""Classes (Object-oriented programming)  
 Object-oriented programming is one of the most effective approaches to writing
 software. In object-oriented programming you write classes that represents real-world
 things and situations, and you create objects based on these classes. Making an object
