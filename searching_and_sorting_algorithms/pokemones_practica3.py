cc91fc04c420d501d0ae9ca98be67d6b5b6dbc31
Fco. Alan Menchaca M <alanmenchaca22@outlook.com>
2021-05-15 20:47:49 -0500

    practice 3: implementing the insertion sort algorithm (order from smallest to largest)

---
+++ searching_and_sorting_algorithms/pokemones_practica3.py
@@ -0,0 +1,9 @@
+from algoritmos_ordenamiento.insertion_sort import InsertionSort
+
+pokemon_cplist = input("Ingresa el poder de cada pokemon (CP): ").split()
+pokemon_list = [int(cp) for cp in pokemon_cplist]
+
+InsertionSort.reverse_sort(pokemon_list)
+
+print('Ordenados de mayor a menor: ', end="")
+asd = [print(pokemon, end=", ") for pokemon in pokemon_list]
